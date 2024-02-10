d50c297b96bfdd0619086fbbce5d2551c9a65c03
RickMMA <rcmrmtac@gmail.com>
2024-02-10 00:36:16 +0000

    Refactored code, deleted 'game' class

--- main.py
+++ main.py
@@ -3,60 +3,66 @@ from pygame.locals import *
 from menu_gui import Button
   
  
-class game():
-      def __init__(self):
-            pygame.init()
-            pygame.display.init()
+pygame.init()
+pygame.display.init()
 
-            self.FPS = 2
-            self.FramePerSec = pygame.time.Clock()
-            self.gameState = False
+FPS = 10
+FramePerSec = pygame.time.Clock()
+gameState = False
 
-            # Predefined some colors
-            self.BLUE  = (0, 0, 255)
-            self.RED   = (255, 0, 0)
-            self.GREEN = (0, 255, 0)
-            self.BLACK = (0, 0, 0)
-            self.WHITE = (255, 255, 255)
+# Predefined some colors
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-            # Screen information
-            self.SCREEN_WIDTH = 800
-            self.SCREEN_HEIGHT = 600
+# Screen information
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
-            self.DISPLAYSURFACE = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
-            pygame.display.set_caption("Chess Game")
+DISPLAYSURFACE = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Chess Game")
 
-            self.FONT = pygame.font.SysFont(None, 48)
+FONT = pygame.font.SysFont(None, 48)
+
+
+rec = pygame.Rect(100, 100, 50, 20)
+surf = pygame.Surface((rec.width, rec.height))
+surf.fill((255, 0, 0))
 
             
 
-      def on_event(self, event):
-            pressed_keys = pygame.key.get_pressed()
-            mouse_buttons = pygame.mouse.get_pressed()
-            self._evento = event
-            
-            if event.type == QUIT:
-                  pygame.quit()
-                  sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                  # print(event.get())
-                  pass
-            elif mouse_buttons[0] == True: # left click
-                  pass                  
-            elif event.type == pygame.KEYDOWN:
-                  if event.key == pygame.K_ESCAPE:
-                        if self.gameState == True:
-                              self.gameState = False
-                        else:
-                              self.gameState = True 
+def on_event(event):
+    pressed_keys = pygame.key.get_pressed()
+    pressed_mouse = pygame.mouse.get_pressed()
+    mouse_pos = pygame.mouse.get_pos()
+
+    global gameState
+    
+    if event.type == QUIT:
+        pygame.quit()
+        sys.exit()
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+        pass
+    elif event.type == pygame.MOUSEMOTION:
+        rec.move_ip(mouse_pos[0], mouse_pos[1])
+    elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_q:
+            if gameState == True:
+                gameState = False
+            else:
+                gameState = True
+        elif pressed_keys[K_ESCAPE]:        
+            print(mouse_pos)
+
+    pygame.event.pump()
+                        
                         
                   
             
-
-
-class chessPiece(pygame.sprite.Sprite, game):
+class chessPiece(pygame.sprite.Sprite):
       def __init__(self, pieceName):
-            super().__init__()
             self.image = pygame.image.load(os.path.join(
                   os.getcwd(), "assets", "chess_pieces", f"{pieceName}.png"))
             self.rect = self.image.get_rect()
@@ -85,39 +91,33 @@ class boardTile():
       def render(self, boardState):
             self.letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
             self.lettersToInt = [65, 66, 67, 68, 69, 70, 71, 72]
-            self.indexes = [1, 2, 3, 4, 5, 6, 7, 8]
-
-            for row in boardState:
-                  for tile in row:
-                        pass
+            self.indexes = [1, 2, 3, 4, 5, 6, 7, 8] 
 
 
       
       
-class chessBoard(pygame.sprite.Sprite, game):
+class chessBoard(pygame.sprite.Sprite):
       def __init__(self):
             super(pygame.sprite.Sprite, self).__init__()
-            super().__init__()
             # each cell is 32x32px, the board is 8cells x 8cells = 256x256px
             self.image = pygame.image.load(os.path.join(os.getcwd(), "assets", "BOARD.png"))
             self.brownTile = pygame.image.load(os.path.join(os.getcwd(), "assets", "brown-tile.png"))
             self.whiteTile = pygame.image.load(os.path.join(os.getcwd(), "assets", "white-tile.png"))
             self.rect = self.image.get_rect()
-            self.position = (self.SCREEN_WIDTH/10, self.SCREEN_HEIGHT/8)
+            self.position = (SCREEN_WIDTH/10, SCREEN_HEIGHT/8)
             self.rect.topleft = self.position
-            self.starting_board = [
-                  ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-                  ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
-                  ["--", "--", "--", "--", "--", "--", "--", "--"],
-                  ["--", "--", "--", "--", "--", "--", "--", "--"],
-                  ["--", "--", "--", "--", "--", "--", "--", "--"],
-                  ["--", "--", "--", "--", "--", "--", "--", "--"],
-                  ["wB", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
-                  ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"]
-            ]
 
-            self.a = boardTile("aa", "bb")
-            print(self.a.piece)
+            self.starting_board = {
+                  "A8":("bR", (0, 0)), "B8":"bN", "C8":"bB", "D8":"bQ", "E8":"bK", "F8":"bB", "G8":"bN", "H8":"bR",
+                  "A7":"bP", "B7":"bP", "C7":"bP", "D7":"bP", "E7":"bP", "F7":"bP", "G7":"bP", "H7":"bP",
+                  "A6":"--", "B6":"--", "C6":"--", "D6":"--", "E6":"--", "F6":"--", "G6":"--", "H6":"--",
+                  "A5":"--", "B5":"--", "C5":"--", "D5":"--", "E5":"--", "F5":"--", "G5":"--", "H5":"--",
+                  "A4":"--", "B4":"--", "C4":"--", "D4":"--", "E4":"--", "F4":"--", "G4":"--", "H4":"--",
+                  "A3":"--", "B3":"--", "C3":"--", "D3":"--", "E3":"--", "F3":"--", "G3":"--", "H3":"--",
+                  "A2":"wP", "B2":"wP", "C2":"wP", "D2":"wP", "E2":"wP", "F2":"wP", "G2":"wP", "H2":"wP",
+                  "A1":"wB", "B1":"wN", "C1":"wB", "D1":"wQ", "E1":"wK", "F1":"wB", "G1":"wN", "H1":"wR"
+            }
+
 
  
       def update(self):
@@ -129,21 +129,13 @@ class chessBoard(pygame.sprite.Sprite, game):
       
  
       def render(self, surface):
-            self.font = self.FONT.render("HOLA MUNDO", False, self.WHITE)
+            self.font = FONT.render("HOLA MUNDO", False, WHITE)
             
             surface.blit(self.image, self.rect) # board image
             surface.blit(self.font, (self.position[0], self.position[1]))
 
-            
-
-            
-            # for row in self.starting_board:
-                  # for i in row:
-                        # print(i)
-
 
       def on_event(self, event):
-            super().on_event(event)
             if mouse_buttons[0] == True: # left click
                   print("eee")
 
@@ -154,35 +146,51 @@ class chessBoard(pygame.sprite.Sprite, game):
 
 
 if __name__ == "__main__":
-      g = game()
       board = chessBoard()
 
       boardState = board.starting_board
 
       img = pygame.image.load(os.path.join(os.getcwd(), "assets", "chess-1280.jpg"))
-      img = pygame.transform.scale(img, (g.SCREEN_WIDTH, g.SCREEN_HEIGHT))
+      img = pygame.transform.scale(img, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
-      btn1 = Button(g.DISPLAYSURFACE, pos = (g.SCREEN_WIDTH/2, g.SCREEN_HEIGHT/2), text="Jugar")
-      btn1.x = g.SCREEN_WIDTH/2 - btn1.size[0]/2  # centers button
-      btn1.y = g.SCREEN_HEIGHT/2 - btn1.size[1]/2
+      btn1 = Button(DISPLAYSURFACE, pos = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2), text="Jugar", font=FONT)
+      btn1.x = SCREEN_WIDTH/2 - btn1.size[0]/2  # centers button
+      btn1.y = SCREEN_HEIGHT/2 - btn1.size[1]/2
       title_font = pygame.font.SysFont(None, 100)
+
+      # rec = pygame.Rect(100, 100, 50, 20)
+      # surf = pygame.Surface((rec.width, rec.height))
+      # surf.fill((255, 0, 0))
       while True:
+            # keys = pygame.key.get_pressed()
+            # mouse_pos = pygame.mouse.get_pos()
+            # mouse_buttons = pygame.mouse.get_pressed()
+            
             for event in pygame.event.get():
-                  g.on_event(event)
+                  on_event(event)
+                  # if event.type == pygame.MOUSEMOTION:
+                        # mouse_pos = pygame.mouse.get_pos()
+
+            # if keys[K_LEFT]:
+                  # rec.move_ip(-10, 0)
+            # elif keys[K_RIGHT]:
+                  # rec.move_ip(10, 0)
+            # elif keys[K_ESCAPE]:
+                  # pass
             
             board.update()
             # piece.move()
-            if g.gameState == False: # When the game is paused, i.e.: in the start menu
-                  g.DISPLAYSURFACE.blit(img, (0, 0))
+            if gameState == False: # When the game is paused, i.e.: in the start menu
+                  DISPLAYSURFACE.blit(img, (0, 0))
                   btn1.render()
-                  f = title_font.render("pyChess", True, g.WHITE)
-                  g.DISPLAYSURFACE.blit(f, (g.SCREEN_WIDTH/2 - f.get_width()/2, g.SCREEN_HEIGHT/4))
+                  f = title_font.render("pyChess", True, WHITE)
+                  DISPLAYSURFACE.blit(f, (SCREEN_WIDTH/2 - f.get_width()/2, SCREEN_HEIGHT/4))
+                  # DISPLAYSURFACE.blit(surf, (rec.x, rec.y) )
             else:
-                  g.DISPLAYSURFACE.fill((200, 200, 200))
-                  board.render(g.DISPLAYSURFACE)
-            # piece.render(game.DISPLAYSURFACE)
+                  DISPLAYSURFACE.fill((200, 200, 200))
+                  board.render(DISPLAYSURFACE)
 
 
             pygame.display.update()
-            g.FramePerSec.tick(g.FPS)
+            FramePerSec.tick(FPS)
 
